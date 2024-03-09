09f1d5370007c0495987d16c25a4fd4899b357d8
cm <54995353+cactobyte@users.noreply.github.com>
2024-03-09 19:53:41 +0000

    Create Race.java

---
+++ Race.java
@@ -0,0 +1,15 @@
+public class Race {
+	private String name;
+	private String description;
+	private int raceID;
+
+	public String getName(){
+		return name;
+	}
+
+	//contructor
+	public race(String name, String description){
+		this.name = name;
+		this.description = description;
+	}
+}
