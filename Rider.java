1a4ff9094ac91a19a7f00a3f020efdf8baa30ea6
OneFishyBoi <116510159+OneFishyBoi@users.noreply.github.com>
2024-03-09 20:38:00 +0000

    Add files via upload
    added rider class

---
+++ Rider.java
@@ -0,0 +1,13 @@
+public class Rider{
+	// attributes
+	private int teamID;
+	private String name;
+	private int yearOfBirth;
+
+	// constructor
+	public Rider(int teamID, String name, int yearOfBirth){
+		this.teamID = teamID;
+		this.name = name;
+		this.yearOfBirth = yearOfBirth;
+	}
+}
