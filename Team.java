406c4bc5ba0c47e29aeaaa26152823ca97597852
OneFishyBoi <116510159+OneFishyBoi@users.noreply.github.com>
2024-03-09 11:45:44 +0000

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,15 @@
+public class Team {
+	private String name;
+	private String description;
+	private int teamID;
+
+	public String getName(){
+		return name;
+	}
+
+	//contructor
+	public team(String name, String description){
+		this.name = name;
+		this.description = description;
+	}
+}
