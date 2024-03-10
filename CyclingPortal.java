7e4ab7814379ea81e90317c207e4708924daac24
cm <54995353+cactobyte@users.noreply.github.com>
2024-03-10 23:23:42 +0000

    Update CyclingPortal.java
    Added createRace

--- CyclingPortal.java
+++ CyclingPortal.java
@@ -17,6 +17,8 @@ public class CyclingPortal implements CyclingPortalInterface {
 	private HashMap<Integer, Team> teamHash = new HashMap<Integer, Team>();
 	private Hashmap<Integer, Rider> riderHash = new HashMap<Integer, Rider>();
 
+	private HashMap<Integer, Race> raceHash = new HashMap<Integer, Race>();
+
 	@Override
 	public int createTeam(String name, String description) throws IllegalNameException, InvalidNameException {
 		// IllegalNameException
@@ -109,5 +111,39 @@ public class CyclingPortal implements CyclingPortalInterface {
 		}
 
 		return Collections.max(riderHash.keySet());
+	}
+	public int createRace(String name, String description) throws IllegalNameException, InvalidNameException {
+		// InvalidNameException
+		if (name == null){
+			throw new InvalidNameException("Name is null");
+		} else if (name.isEmpty()){
+			throw new InvalidNameException("Name is empty");
+		} else if (name.length() > 30){
+			throw new InvalidNameException("Name is too long");
+		} else if (name.contains(" ")){
+			throw new InvalidNameException("Name contains whitespace");
+		}
+
+		// IllegalNameException
+		// looping through all race checking if name exists
+		// id is key, value is race *del*
+		for (String i : raceHash.values()) {
+			if (name == i){
+				throw new IllegalNameException("Race name already exists");
+			}
+		}
+
+		// main method logic
+		Race newRace = new Race(name, description);
+		// assigning ID to race, ******NEED EDITING****
+		int raceId = raceHash.size()
+		if (raceId == 0){
+			raceHash.put(0, newRace);
+		} else {
+			raceHash.put(Collections.max(raceHash.keySet()) + 1, newRace);
+		}
+
+		return Collections.max(teamHash.keySet()); // can this not just be raceId + 1 ??? 
+
 	}
 }
