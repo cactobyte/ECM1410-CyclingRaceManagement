16800de5f07ac2caff73c7d70b32b25331ee8ecb
cm <54995353+cactobyte@users.noreply.github.com>
2024-03-13 16:47:38 +0000

    Create Stage.java

---
+++ Coursework/cycling/Stage.java
@@ -0,0 +1,27 @@
+package cycling;
+
+import java.time.LocalDateTime;
+
+public class Stage {
+	private int raceID;
+	private String stageName;
+	private String description;
+	private double length;
+	private java.time.LocalDateTime startTime,
+	private StageType type;
+
+	public String getName(){
+		return name;
+	}
+
+	//contructor
+	public Stage(int raceID, String stageName, String description, double length, java.time.LocalDateTime startTime, StageType type){
+		this.raceId = raceID;
+		this.stageName = stageName;
+		this.description = description;
+		this.length = length;
+		this.startTime = startTime;
+		this.type = type;
+	}
+
+}
