f023cc5609d2945497a5c408cfb55c498e4926fb
cm <54995353+cactobyte@users.noreply.github.com>
2024-03-19 19:13:49 +0000

    Update TEMP BORIS FILE.java

--- TEMP BORIS FILE.java
+++ TEMP BORIS FILE.java
@@ -14,13 +14,6 @@
 */
 
 // bruh im not cooking here
-
-
-// This is a map where the key is the stageId, and the value is another map
-// The inside/inner map stores the rider id and the result time, ill call it resultsHash, but technically its a big stageRiderResultHash lol
-private Map<Integer, Map<Integer, LocalTime[]>> resultsHash = new HashMap<>();
-
-
 @Override 														// Idk what these three dots are but its in all the documentations???? IDK
 public void registerRiderResultsInStage(int stageId, int riderId, LocalTime... checkpointTimes) 
 throws IDNotRecognisedException, DuplicatedResultException, InvalidCheckpointTimesException, InvalidStageStateException {
@@ -36,10 +29,6 @@ throws IDNotRecognisedException, DuplicatedResultException, InvalidCheckpointTim
 		throw new InvalidStageStateException("Stage is not at the correct state"); // Sounds dodgy, change in future
 	}
 
-	if (resultsHash.containsKey(stageId) && resultsHash.get(stageId).containsKey(riderId)) {
-		throw new DuplicatedResultException("Rider already has a result during this stage");
-	}
-
 	// Lowkey need help with this one...
 	/* 
 	@throws InvalidCheckpointTimesException Thrown if the length of checkpointTimes is
@@ -48,13 +37,11 @@ throws IDNotRecognisedException, DuplicatedResultException, InvalidCheckpointTim
  *                                     the start time and the finish time of the
 	*/
 
-
-	if (!resultsHash.containsKey(stageId)) { // could use putIfAbsent
-	    resultsHash.put(stageId, new HashMap<>()); // Creates a hash with stageid and another empty hashmap
+	Rider rider = riderHash.get(riderId);
+	if (rider.addStageResult().containsKey(stageId)){
+		throw new DuplicatedResultException("Rider already has a result for this stage");
 	}
-	resultsHash.get(stageId).put(riderId, checkpointTimes); // Adds the rider id and corresponding time to that "inner" hashmap
+	rider.addStageResult(stageId, checkpointTimes);
+	
 
 }
-
-
-
