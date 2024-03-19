23a01037c6326445348b7c218ccbb21b2a8e1eb9
cm <54995353+cactobyte@users.noreply.github.com>
2024-03-19 17:21:29 +0000

    Update TEMP BORIS FILE.java

--- TEMP BORIS FILE.java
+++ TEMP BORIS FILE.java
@@ -13,43 +13,48 @@
 
 */
 
-// For Testing
-
-// **************************** IN THE FUTURE: Make sure remove team also removes riders in that team (when its implemented) *********************
+// bruh im not cooking here
 
 
-// Checking if portals are independant of one another
-try {
-    teamTwo = portal1.createTeam("TeamTwo", "Another team");
-} catch (IllegalNameException | InvalidNameException e) {
-    e.printStackTrace();
+// This is a map where the key is the stageId, and the value is another map
+// The inside/inner map stores the rider id and the result time, ill call it resultsHash, but technically its a big stageRiderResultHash lol
+private Map<Integer, Map<Integer, LocalTime[]>> resultsHash = new HashMap<>();
+
+
+@Override 														// Idk what these three dots are but its in all the documentations???? IDK
+public void registerRiderResultsInStage(int stageId, int riderId, LocalTime... checkpointTimes) 
+throws IDNotRecognisedException, DuplicatedResultException, InvalidCheckpointTimesException, InvalidStageStateException {
+	if (!stageHash.containsKey(stageId)){
+		throw new IDNotRecognisedException("Stage ID not in system");
+	}
+	if (!riderHash.containsKey(riderId)){
+		throw new IDNotRecognisedException("Rider ID not in system");
+	}
+
+	Stage stage = stageHash.get(stageId);
+	if (stageHash.get(stageId).getStageState() != "waiting for results"){
+		throw new InvalidStageStateException("Stage is not at the correct state"); // Sounds dodgy, change in future
+	}
+
+	if (resultsHash.containsKey(stageId) && resultsHash.get(stageId).containsKey(riderId)) {
+		throw new DuplicatedResultException("Rider already has a result during this stage");
+	}
+
+	// Lowkey need help with this one...
+	/* 
+	@throws InvalidCheckpointTimesException Thrown if the length of checkpointTimes is
+ *                                     not equal to n+2, where n is the number
+ *                                     of checkpoints in the stage; +2 represents
+ *                                     the start time and the finish time of the
+	*/
+
+
+	if (!resultsHash.containsKey(stageId)) { // could use putIfAbsent
+	    resultsHash.put(stageId, new HashMap<>()); // Creates a hash with stageid and another empty hashmap
+	}
+	resultsHash.get(stageId).put(riderId, checkpointTimes); // Adds the rider id and corresponding time to that "inner" hashmap
+
 }
-assert (portal1.getTeams().length == 2) : "Portal1 should have two teams.";
-assert (portal2.getTeams().length == 1) : "Portal2 should have one team.";
-
-// Checking remove team
-try {
-	portal1.removeTeam(teamTwo);
-} catch (IDNotRecognisedException e){
-	e.printStackTrace();
-}
-assert (portal1.getTeams().length == 1) : "Portal1 should have one team.";
 
 
-
-// Checking createRider
-try{
-	teams = portal1.getTeams(); // Gets Team ID's from portal1
-	rider = portal1.createRider(teams[0], "Bob Smith", 2000); // Created Rider, returns rider id
-} catch (IDNotRecognisedException e) {
-	e.printStackTrace();
-} catch (IllegalArgumentException e) {
-	e.printStackTrace();
-}
-
-// Checking if Rider was created into the correct team.
-
-
-
-assert(portal1.removeRider(rider)) : "ID could not be removed";
 
